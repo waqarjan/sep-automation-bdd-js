02cd1aa0e5ba4ce8b140955b1d803c46b2d3b0ed
AJan <wakarjan@hotmail.com>
2026-08-23 18:19:48 -0400

    updates to BasePage

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -11,18 +11,18 @@ export class BasePage {
   }
 
   async login() {
-    // await this.page.context().setHTTPCredentials({
-    //   username: process.env.SEP_USERNAME,
-    //   password: process.env.SEP_PASSWORD,
-    // });
-    // await this.page.goto(process.env.SEP_QA_URL);
-    // BrowserUtility.verify_title(this.page, 'Checkout | Cydeo');
-    // await this.page.waitForLoadState('networkidle');
-
-    const code = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
-    await this.page.setExtraHTTPHeaders({Authorization: `Basic ${code}`,});
+    await this.page.context().setHTTPCredentials({
+      username: process.env.SEP_USERNAME,
+      password: process.env.SEP_PASSWORD,
+    });
     await this.page.goto(process.env.SEP_QA_URL);
     BrowserUtility.verify_title(this.page, 'Checkout | Cydeo');
-    await this.page.waitForLoadState("networkidle"); 
+    await this.page.waitForLoadState('networkidle');
+
+    // const code = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
+    // await this.page.setExtraHTTPHeaders({Authorization: `Basic ${code}`,});
+    // await this.page.goto(process.env.SEP_QA_URL);
+    // BrowserUtility.verify_title(this.page, 'Checkout | Cydeo');
+    // await this.page.waitForLoadState("networkidle"); 
   }
 }
